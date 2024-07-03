da1ca48972bca0fb63a0a9b9484681c983933672
Reece <reece.cares@gmail.com>
2024-07-03 21:33:25 +1000

    Initialising maven, maven run files, .gitignore

---
+++ src/test/java/MK/CVForYou/AppTest.java
@@ -0,0 +1,20 @@
+package MK.CVForYou;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
