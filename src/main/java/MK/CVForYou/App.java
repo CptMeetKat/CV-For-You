f30751cd8bbbcdaf5b124255bfe32638116b334e
Reece <reece.cares@gmail.com>
2024-07-04 20:35:00 +1000

    Added remove punctutation, split math functions

--- src/main/java/MK/CVForYou/App.java
+++ src/main/java/MK/CVForYou/App.java
@@ -13,7 +13,18 @@ public class App
 
         cosineSimilarity(v1,v2);
 
-         
+
+
+        String test = "hello, neo the matrix has you";
+        System.out.println(removePunctuation(test));
+        String[] atoms = test.split(" ");
+
+
+
+        //HashMap<String, int> word_table = wordsToMap();
+
+
+
 
     }
 
@@ -45,16 +56,21 @@ public class App
         return Math.sqrt(result);
     }
 
+    public static int dotProduct(int v1[], int v2[])
+    {
+        int dot_product = 0;
+        for (int i = 0; i < v1.length; i++) {
+            dot_product += v1[i] * v2[i];
+        }
+        return dot_product;
+    }
+
     public static double cosineSimilarity(int v1[], int v2[])
     {
         if( v1.length != v2.length)
             return -1;
 
-        int dot_product = 0;
-
-        for (int i = 0; i < v1.length; i++) {
-            dot_product += v1[i] * v2[i];
-        }
+        int dot_product = dotProduct(v1,v2);
 
         double v1Magnitude = magnitude(v1);
         double v2Magnitude = magnitude(v2);
@@ -69,8 +85,23 @@ public class App
         return result;
     }
 
+    public static String removePunctuation(String s)
+    {
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 0; i < sb.length(); i++) {
+           char letter = sb.charAt(i); 
+
+           if(  !Character.isAlphabetic(letter) && 
+                !Character.isSpaceChar(letter))
+                sb.deleteCharAt(i);
+
+        }
+        return sb.toString();
+    }
+
     public HashMap<String,Integer> wordsToMap(String text)
     {
+        
         return new HashMap<String, Integer>();
     }
 }
