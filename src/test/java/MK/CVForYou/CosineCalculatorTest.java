4d56308527305b5de8a1796b7c596c1ba6504b8e
Reece <reece.cares@gmail.com>
2024-07-05 21:00:45 +1000

    Added test for magnitude

--- src/test/java/MK/CVForYou/CosineCalculatorTest.java
+++ src/test/java/MK/CVForYou/CosineCalculatorTest.java
@@ -17,4 +17,18 @@ public class CosineCalculatorTest
         long b = Math.round(result * 10000.0);
         assertTrue( a == b );
     }
+
+
+    @Test
+    public void shouldCalculateMagnitude()
+    {
+        int [] vector = new int[]{1,1,1,1,1,0,0};
+        double result = CosineCalculator.magnitude(vector);
+        double expected = 2.2360679775;
+
+        long a = Math.round(expected * 10000.0);
+        long b = Math.round(result * 10000.0);
+
+        assertTrue( a == b );
+    }
 }
