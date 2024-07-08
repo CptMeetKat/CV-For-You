5236123953c7035baf69c087debd8038268e3c13
Reece <reece.cares@gmail.com>
2024-07-08 19:29:06 +1000

    Added json-java dependency

--- src/main/java/MK/CVForYou/App.java
+++ src/main/java/MK/CVForYou/App.java
@@ -1,13 +1,20 @@
 package MK.CVForYou;
+import org.json.JSONObject;
+
+
+
 
 public class App 
 {
     public static void main( String[] args )
     {
-        CosineCalculator.calculate("hello, neo the matrix has you", 
-                "neo, there is a glitch in the matrix");
+        //CosineCalculator.calculate("hello, neo the matrix has you", 
+        //        "neo, there is a glitch in the matrix");
 
-        CosineCalculator.calculate("the best data science course",
-                "data science is popular");
+        //CosineCalculator.calculate("the best data science course",
+        //        "data science is popular");
+
+       JSONObject jo = new JSONObject("{ \"abc\" : \"def\" }");
+       System.out.println(jo);
     }
 }
