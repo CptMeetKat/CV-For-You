5a8f8a7b0d861236d3639a1529417bfd78d20471
Reece <reece.cares@gmail.com>
2024-07-19 00:10:00 +1000

    Testing for arg policies

---
+++ src/test/java/MK/CVForYou/ArgParserTest.java
@@ -0,0 +1,61 @@
+package MK.CVForYou;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class ArgParserTest 
+{
+    @Test
+    public void shouldNotHaveMoreThanOneCompareSources()
+    {
+        String[] args = new String[]{"-d", "CV_template.html",
+                                     "-cs", "https://www.seek.com.au/job/00000001",
+                                     "-c", "compare_file.txt",
+                                     "-s", "section1.json", "section2.json",
+                                     "-o", "output.out/"};
+
+        ArgParser ap = new ArgParser();
+        boolean result = ap.parseArgs(args);
+        assertFalse(result);
+    }
+
+    @Test
+    public void shouldReturnFalseWhenMissingCompareFlag()
+    {
+        String[] args = new String[]{"-d", "CV_template.html",
+                                     "-s", "section1.json", "section2.json",
+                                     };
+
+        ArgParser ap = new ArgParser();
+        boolean result = ap.parseArgs(args);
+        assertFalse(result);
+    }
+
+
+    @Test
+    public void shouldReturnFalseWhenMissingDocumentFlag()
+    {
+        String[] args = new String[]{"-c", "compare_file.txt",
+                                     "-s", "section1.json", "section2.json",
+                                     };
+
+        ArgParser ap = new ArgParser();
+        boolean result = ap.parseArgs(args);
+        assertFalse(result);
+    }
+
+
+    @Test
+    public void shouldReturnFalseWhenMissingSectionFlag()
+    {
+        String[] args = new String[]{"-d", "CV_template.html",
+                                     "-c", "compare_file.txt",
+                                     };
+
+        ArgParser ap = new ArgParser();
+        boolean result = ap.parseArgs(args);
+        assertFalse(result);
+    }
+
+}
