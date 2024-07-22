857a7353d8b761b3662a8568185bb9fe113789aa
Reece <reece.cares@gmail.com>
2024-07-22 22:35:13 +1000

    Deserialised SavedJobs response from Seek

--- src/main/java/MK/CVForYou/SeekSavedJob.java
+++ src/main/java/MK/CVForYou/SeekSavedJob.java
@@ -5,30 +5,40 @@ import org.json.JSONObject;
 public class SeekSavedJob
 {
     String job_id;
-    //isActive
-    //notes
-    //isExternal
-    //title
-    //location
-    //abstract
-    //recruiter
-    //salary
-    //
+    boolean isActive;
+    String notes;
+    boolean isExternal;
+    String title;
+    String location;
+
+    String job_abstract;
+    String company;
+    String salary;
     public SeekSavedJob(JSONObject node)
     {
-        job_id = (String) node.get("id");
-        //System.out.println(job_id);
-        //System.out.println((boolean)node.get("isActive"));
-        
-        ///System.out.println((boolean) null);
-        //isActive = node.get(
+        job_id = node.optString("id");
+        isActive = (boolean) node.get("isActive");
+        notes = node.optString(notes);
+        isExternal = (boolean) node.get("isExternal");
+
+
+        JSONObject job = (JSONObject) node.query("/job");
+        title = job.optString("title");
+        location = job.optString("location");
+        job_abstract = job.optString("abstract");
+
+        JSONObject advertiser = (JSONObject) job.getJSONObject("advertiser");
+        company = advertiser.optString("name");
+
+
+        JSONObject salaryJson = job.optJSONObject("salary");
+        if (salaryJson != null) {
+            salary = salaryJson.optString("label", null);
+        }
+
+
+
     }
-
-    public void printMe()
-    {
-
-    }
-
 
     public String getID()
     {
