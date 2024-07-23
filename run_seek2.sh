6af8e531ddef12066b4f73f3f421cd1fe1560356
Reece <reece.cares@gmail.com>
2024-07-23 20:59:27 +1000

    Added quiet mode to run script

--- run_seek2.sh
+++ run_seek2.sh
@@ -2,6 +2,6 @@
 
 echo "WARNING: you may be required to change the seek URL in this script for future versions"
 
-mvn clean compile exec:java -Dexec.args="-d prod_components/CV_Pretty.html -ca -s prod_components/tags1.json prod_components/tags2.json prod_components/projects.json -o prod_components/"
+mvn -q clean compile exec:java -Dexec.args="-d prod_components/CV_Pretty.html -ca -s prod_components/tags1.json prod_components/tags2.json prod_components/projects.json -o prod_components/"
 
 
