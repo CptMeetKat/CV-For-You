5ca02f50a37bbe67e05abc56f28e53907a58657f
Reece <reece.cares@gmail.com>
2024-07-24 23:38:26 +1000

    Removed old warning message

--- run_seek2.sh
+++ run_seek2.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "WARNING: you may be required to change the seek URL in this script for future versions"
-
 mvn -q clean compile exec:java -Dexec.args="-d prod_components/CV_Pretty.html -ca -s prod_components/tags1.json prod_components/tags2.json prod_components/projects.json -o prod_components/"
 
 
