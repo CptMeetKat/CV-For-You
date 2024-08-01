56f7068c297ece20af29b07b331ace60762ac797
Reece <reece.cares@gmail.com>
2024-08-01 22:52:38 +1000

    Updated quick run files

--- run_seek.sh
+++ run_seek.sh
@@ -2,6 +2,6 @@
 
 echo "WARNING: you may be required to change the seek URL in this script for future versions"
 
-mvn clean compile exec:java -Dexec.args="-d prod_components/CV_Pretty.html -cs https://www.seek.com.au/job/76113399 -s prod_components/tags1.json prod_components/tags2.json prod_components/projects.json -o prod_components/"
+mvn clean compile exec:java -Dexec.args="-d assets/prod_components/CV_Pretty.html -cs https://www.seek.com.au/job/76113399 -s assets/prod_components/tags1.json assets/prod_components/tags2.json assets/prod_components/projects.json -o assets/prod_components/"
 
 
--- run_seek2.sh
+++ run_seek2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mvn -q clean compile exec:java -Dexec.args="-d prod_components/CV_Pretty.html -ca -s prod_components/tags1.json prod_components/tags2.json prod_components/projects.json -o prod_components/"
+mvn -q clean compile exec:java -Dexec.args="-d assets/prod_components/CV_Pretty.html -ca -s assets/prod_components/tags1.json assets/prod_components/tags2.json assets/prod_components/projects.json -o assets/prod_components/"
 
 
