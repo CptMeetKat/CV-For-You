61b4f79fb1d3d134aed9b09b29cded2eaf8f2380
Reece <reece.cares@gmail.com>
2024-08-02 23:17:07 +1000

    Refactor functions in main - To reduce function leakiness / coupling

--- src/main/java/MK/CVForYou/App.java
+++ src/main/java/MK/CVForYou/App.java
@@ -14,7 +14,7 @@ public class App
 
     public App(ArgParser ap)
     {
-        HashMap<String, String> job_descriptions = getJobDescriptions(ap);
+        HashMap<String, String> job_descriptions = getJobDescriptions(ap.getJDSource());
 
         for (String job_id: job_descriptions.keySet()) 
         {
@@ -26,19 +26,8 @@ public class App
         }
     }
 
-    public static HashMap<String, String> getJobDescriptions(ArgParser ap)
+    public static HashMap<String, String> getJobDescriptions(JobDescriptionSource jd_source)
     {
-        String source = ap.document_source;
-        JobDescriptionSource jd_source = null;
-
-        if(source.equals("seek"))
-            jd_source = new JobDescriptionFromSeekJob(ap.seek_url);
-        else if(source.equals("file"))
-            jd_source = new JobDescriptionFromFile(ap.compare_document_path);
-        else if(source.equals("seek_saved"))
-            jd_source = new JobDescriptionFromSaved();
-
-//TODO: Do we care about SRP here?
         HashMap<String, String> job_descriptions = new HashMap<String, String>();
         ArrayList<InputJob> jobs = jd_source.getJobDescriptions();
         for (InputJob job : jobs) {
