2f45378d9e6c5372020596006d305f7d03de5be5
Reece <reece.cares@gmail.com>
2024-08-06 16:22:50 +1000

    Removed 2 production files

--- run_seek.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "WARNING: you may be required to change the seek URL in this script for future versions"
-
-mvn clean compile exec:java -Dexec.args="-d assets/prod_components/CV_Pretty.html -cs https://www.seek.com.au/job/76113399 -s assets/prod_components/tags1.json assets/prod_components/tags2.json assets/prod_components/projects.json -o assets/prod_components/"
-
-
--- run_seek2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mvn -q clean compile exec:java -Dexec.args="-d assets/prod_components/CV_Pretty.html -ca -s assets/prod_components/tags1.json assets/prod_components/tags2.json assets/prod_components/projects.json -o assets/prod_components/"
-
-
