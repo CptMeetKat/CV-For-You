7fb2985bea8679f26d2cdeabfe7c8298d5ab0f24
Reece <reece.cares@gmail.com>
2024-08-09 18:17:38 +1000

    Quickrun file will grab all sections in folder

--- run_sample.sh
+++ run_sample.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
-mvn clean compile exec:java -Dexec.args="-d assets/sample_components/document.html -c assets/sample_components/description1.txt -s assets/sample_components/projects.json assets/sample_components/tags.json"
+sections=$(echo assets/sample_components/sections/*.json)
+mvn clean compile exec:java -Dexec.args="-d assets/sample_components/document.html -c assets/sample_components/description1.txt -s $sections"
 
 
