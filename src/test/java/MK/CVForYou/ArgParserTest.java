ff2c05a88df44e1d4402098f05f4d318480d9b91
Reece <reece.cares@gmail.com>
2024-08-10 13:36:18 +1000

    Added test for -sd flag

--- src/test/java/MK/CVForYou/ArgParserTest.java
+++ src/test/java/MK/CVForYou/ArgParserTest.java
@@ -88,6 +88,30 @@ public class ArgParserTest
     }
 
 
+    @Test
+    public void sectionDirectoryFlagShouldReturnAllJSONFilesInMultipleDirectories()
+    {
+        String[] args = new String[]{"-d", "CV_template.html",
+                                     "-c", "compare_file.txt",
+                                     "-sd", "src/test/test_files/ArgParser/directory1/",
+                                            "src/test/test_files/ArgParser/directory2/",
+                                     };
+
+        String[] expected = new String[]{"src/test/test_files/ArgParser/directory1/A.json",
+                                         "src/test/test_files/ArgParser/directory1/B.json",
+                                         "src/test/test_files/ArgParser/directory2/C.json"
+        };
+
+        ArgParser ap = new ArgParser();
+        ap.parseArgs(args);
+        String[] result = ap.getSections();
+        
+        if(expected.length != result.length)
+            fail("Expected and result differ in total files");
+        for(int i = 0; i < expected.length; i++)
+            assertEquals(expected[i], result[i]);
+    }
+
 
 
 
