145b0dd5ddb144260ae525304b941f5152cfdb0a
Reece <reece.cares@gmail.com>
2024-09-06 19:51:31 +1000

    Added test for ReplaceableKey constructor

--- src/test/java/MK/CVForYou/ReplaceableKeyTest.java
+++ src/test/java/MK/CVForYou/ReplaceableKeyTest.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class ReplaceableKeyTest 
 {
     @Test
-    public void keyWithMulipleFieldsShouldHaveKeyAndMulipleFieldsAndSection()
+    public void keyWithMulipleFieldsShouldHaveMulipleFieldsAndSection()
     {
         ReplaceableKey key = new ReplaceableKey("{$title(job_title,job_description)}");
         ArrayList<String> fields = key.getFields();
@@ -21,7 +21,19 @@ public class ReplaceableKeyTest
         assertEquals(fields.get(1), "job_description");
     }
 
-    //{$section_name1(job_description)}
+    @Test
+    public void keyWithSingleFieldShouldHaveSingleFieldAndSection()
+    {
+        ReplaceableKey key = new ReplaceableKey("{$title(job_description)}");
+        ArrayList<String> fields = key.getFields();
+        String section_name = key.getSectionName();
+
+        assertEquals("title", section_name);
+        assertEquals(fields.size(), 1);
+        assertEquals(fields.get(0), "job_description");
+    }
+
+
     //{$section_name1()}
     //{$section_name1}
     //INVALID
