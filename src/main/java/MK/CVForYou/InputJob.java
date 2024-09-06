a38dfc90625965c66041c93a8b44d9d7bd66ff00
Reece <reece.cares@gmail.com>
2024-09-06 22:57:39 +1000

    Make InputJob more struct-like

--- src/main/java/MK/CVForYou/InputJob.java
+++ src/main/java/MK/CVForYou/InputJob.java
@@ -5,15 +5,10 @@ public class InputJob
 {
     public String name;
     public String job_description;
-    private String job_title;
+    public String job_title;
     public InputJob(String name, String job_description)
     {
         this.name = name;
         this.job_description = job_description;
-    }
-
-    public String getTitle()
-    {
-        return job_title;
     }
 }
