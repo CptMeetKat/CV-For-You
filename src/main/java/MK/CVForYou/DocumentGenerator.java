42b7e9bcbe61697c8a6016cd7b35306af6a18b6b
Reece <reece.cares@gmail.com>
2024-09-06 23:52:08 +1000

    Add quotes to improve readability of log

--- src/main/java/MK/CVForYou/DocumentGenerator.java
+++ src/main/java/MK/CVForYou/DocumentGenerator.java
@@ -81,7 +81,7 @@ public class DocumentGenerator
 				Field f1 = InputJob.class.getField(p);
 				String value = (String)f1.get(job);
                 if(value != null) {
-                    logger.debug("The field '{}' will be used to generate section {}", f1.getName(), replaceable_key.getOriginalKey());
+                    logger.debug("The field '{}' will be used to generate section '{}'", f1.getName(), replaceable_key.getOriginalKey());
                     return value;
                 }
 			} 
