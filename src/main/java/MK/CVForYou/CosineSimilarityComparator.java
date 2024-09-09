eff59ea2a2847dae8b335b054c28c203060b6cab
Reece <reece.cares@gmail.com>
2024-09-09 22:22:06 +1000

    Only calculate the word frequency of job description once (cache)

--- src/main/java/MK/CVForYou/CosineSimilarityComparator.java
+++ src/main/java/MK/CVForYou/CosineSimilarityComparator.java
@@ -1,21 +1,25 @@
 package MK.CVForYou;
 
 import java.util.Comparator;
+import java.util.HashMap;
 
 public class CosineSimilarityComparator implements Comparator<DynamicHTMLElement>
 {
-    String text;
+    final String text;
+    final HashMap<String, Integer> word_table;
 
     public CosineSimilarityComparator(String primary_text)
     {
         text = primary_text;
+        word_table = CosineCalculator.wordsToMap(primary_text);
+
     }
 
 	@Override
 	public int compare(DynamicHTMLElement d1, DynamicHTMLElement d2) {
         
-        double d1_score = CosineCalculator.calculate(text, d1.getKeywords());
-        double d2_score = CosineCalculator.calculate(text, d2.getKeywords());
+        double d1_score = CosineCalculator.calculate(word_table, d1.getKeywords());
+        double d2_score = CosineCalculator.calculate(word_table, d2.getKeywords());
 
         if(d1_score > d2_score) //Most similar is first
             return -1;
