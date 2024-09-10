c9d295affb8a7fb1b5316fbe8081a8ccc6fd80fa
Reece <reece.cares@gmail.com>
2024-09-10 22:31:51 +1000

    Use factory to create work item

--- src/main/java/MK/CVForYou/JobDescriptionFromSeekJob.java
+++ src/main/java/MK/CVForYou/JobDescriptionFromSeekJob.java
@@ -13,18 +13,9 @@ public class JobDescriptionFromSeekJob implements JobDescriptionSource
 
 	@Override
 	public ArrayList<InputJob> getJobModel() {
-        
         ArrayList<InputJob> jobs = new ArrayList<>();
-
-        //TODO: This section of code is duplicate with other JobDescriptionSource...
         SeekJobDescriptionWrapper wrapper = new SeekJobDescriptionWrapper(url, true);
-
-        InputJob work_item = new InputJob();
-        work_item.name = wrapper.getSeekJobID();
-        work_item.job_description = wrapper.getJobDescription();
-        work_item.job_title = wrapper.getJobTitle();
-        jobs.add(work_item);
-
+        jobs.add(InputJobFactory.createWorkItem(wrapper));
         return jobs;
 	}
 
