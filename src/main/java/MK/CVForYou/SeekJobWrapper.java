3f2a0da34955a3f16c6957cef4afc82a0a3a137d
Reece <reece.cares@gmail.com>
2024-09-11 23:08:52 +1000

    SeekSavedJobWrapper uses SeekJobParser methods

--- src/main/java/MK/CVForYou/SeekJobWrapper.java
+++ src/main/java/MK/CVForYou/SeekJobWrapper.java
@@ -1,16 +1,12 @@
 package MK.CVForYou;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.jsoup.select.Elements;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -44,6 +40,7 @@ public class SeekJobWrapper {
         return job_url.substring(slash_position+1); //Unsafe if / is last character
     }
 
+
     public void initialise()
     {
         Document page = getJDPageFromCache(cache_directory, getSeekJobID());
@@ -54,62 +51,9 @@ public class SeekJobWrapper {
             cachePage(page, getSeekJobID(), this.cache_directory); //TODO: this getSeekJobID() function smells funny
             sleep(); //Avoid flagging seek systems
         }
-        job_description = extractJobSectionFromHTML(page);
-        job_title = extractJobTitleFromHTML(page);
+        job_description = SeekJobParser.extractJobSectionFromHTML(page); //SeekJobPageParser //SeekJobDescriptionParser
+        job_title = SeekJobParser.extractJobTitleFromHTML(page);
         logger.trace("Job title detected: {}", job_title);
-    }
-
-    private String extractJobTitleFromHTML(Document doc)
-    {
-        String job_title = null;
-        String server_state = extractServerState(doc, "SK_DL");
-        try {
-            JSONObject json = new JSONObject(server_state);
-            job_title = json.optString("jobTitle");
-        } catch (JSONException e) {
-            logger.warn("Unable to parse Seek server state as JSON: {}", server_state);
-        }
-        return job_title;
-    }
-
-    //OPTIONS: SEEK_APOLLO_DATA, SEEK_CONFIG, SK_DL, SEEK_APP_CONFIG, SEEK_REDUX_DATA
-    //TODO: Make this more dynamic in how it obtains fields
-    private static String extractServerState(Document doc, String field)
-    {
-        Element divElement = doc.select("script[data-automation=server-state]").first();
-        if (divElement != null)
-        {
-            String temp = divElement.data();
-
-            temp = insertNewLineBefore(temp, "window.SEEK_APOLLO_DATA");
-            temp = insertNewLineBefore(temp, "window.SEEK_CONFIG");
-            temp = insertNewLineBefore(temp, "window.SK_DL");
-            temp = insertNewLineBefore(temp, "window.SEEK_APP_CONFIG");
-            temp = insertNewLineBefore(temp, "window.SEEK_REDUX_DATA");
-
-            String[] states = temp.split("\n");
-
-            for (int i = 0; i < states.length; i++) {
-                if(states[i].startsWith("window."+field))
-                {
-                    int json_start = states[i].indexOf("{");
-                    return states[i].substring(json_start);
-                }
-            }
-            logger.warn("Unable to obtain {} from HTML", field);
-        }
-        else 
-        {
-            logger.warn("The <script> element with data-automation='server-state' was not found.");
-        }
-
-        return null;
-
-    }
-
-    private static String insertNewLineBefore(String text, String before)
-    {
-        return text.replace(before, "\n"+before);
     }
 
     public String getJobDescription()
@@ -180,24 +124,6 @@ public class SeekJobWrapper {
         }
 
         return result;
-    }
-
-    private static String extractJobSectionFromHTML(Document doc)
-    {
-        StringBuilder job_description = new StringBuilder();
-        Element divElement = doc.select("div[data-automation=jobAdDetails]").first();
-
-        if (divElement != null)
-        {
-            Elements elements = divElement.getAllElements();
-            for (Element e : elements)
-                job_description.append(e.ownText() + "\n");
-        }
-        else 
-        {
-            logger.warn("The <div> element with data-automation='jobAdDetails' was not found.");
-        }
-        return job_description.toString();
     }
 }
 
