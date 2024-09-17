1bfc2cbcfb33b2f48b130bf214e5e347ab527adb
Reece <reece.cares@gmail.com>
2024-09-17 20:01:51 +1000

    Added missing throws in interface definition

--- src/main/java/MK/CVForYou/Requestable.java
+++ src/main/java/MK/CVForYou/Requestable.java
@@ -1,9 +1,11 @@
 package MK.CVForYou;
 
 
+import java.io.IOException;
+
 import org.json.JSONObject;
 
 public interface Requestable
 {
-    public JSONObject request(String access_token);
+    public JSONObject request(String access_token) throws IOException, InterruptedException;
 }
