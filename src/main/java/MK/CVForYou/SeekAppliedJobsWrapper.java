233ad201331ee8d0bad138288604f9b12f61aabd
Reece <reece.cares@gmail.com>
2024-09-17 21:52:15 +1000

    Added interface code to Applied jobs wrapper

--- src/main/java/MK/CVForYou/SeekAppliedJobsWrapper.java
+++ src/main/java/MK/CVForYou/SeekAppliedJobsWrapper.java
@@ -1,10 +1,25 @@
 
 package MK.CVForYou;
 
-public class SeekAppliedJobsWrapper
+import java.io.IOException;
+
+import org.json.JSONObject;
+
+public class SeekAppliedJobsWrapper implements Requestable
 {
     public SeekAppliedJobsWrapper()
     {
         System.out.println("I am alive!");
+    }
+
+	@Override
+	public JSONObject request(String access_token) throws IOException, InterruptedException {
+        return getAppliedJobs(access_token);
+	}
+
+    public JSONObject getAppliedJobs(String access_token) throws IOException, InterruptedException {
+
+            JSONObject result = null;
+            return result;
     }
 }
