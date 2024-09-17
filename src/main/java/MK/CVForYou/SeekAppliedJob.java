1771b0b07a8feeb7d9ef930c334f5ae08ff927bd
Reece <reece.cares@gmail.com>
2024-09-17 23:36:53 +1000

    Parse Status on AppliedJobs

--- src/main/java/MK/CVForYou/SeekAppliedJob.java
+++ src/main/java/MK/CVForYou/SeekAppliedJob.java
@@ -1,19 +1,22 @@
 package MK.CVForYou;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class SeekAppliedJob
 {
     public String job_id;
     public String job_title;
-    public String status;
+    public ArrayList<String> status;
     public boolean active;
     public String company_name;
     public String company_id;
 
     public SeekAppliedJob(JSONObject node)
     {
-
         job_id = node.optString("id");
 
         JSONObject job = (JSONObject) node.optQuery("/job");
@@ -31,8 +34,22 @@ public class SeekAppliedJob
             company_id = advertiser.optString("id");
         }
 
-        //TODO: status
-        
+        status = new ArrayList<String>(3);
+
+
+        JSONArray events = (JSONArray) node.query("/events");
+        if(events != null)
+        {
+            Iterator<Object> events_itr = events.iterator();
+            while(events_itr.hasNext())
+            {
+                JSONObject event = (JSONObject)events_itr.next();
+                String status_value = event.optString("status");
+                if(status_value != null)
+                    status.add( status_value  );
+            }
+        }
+
     }
 }
 
