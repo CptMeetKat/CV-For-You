1f050d13949324e5162730f76d12b4b84ce61302
Reece <reece.cares@gmail.com>
2024-09-17 23:00:41 +1000

    Add AppliedJob data class

---
+++ src/main/java/MK/CVForYou/SeekAppliedJob.java
@@ -0,0 +1,25 @@
+package MK.CVForYou;
+
+import org.json.JSONObject;
+
+public class SeekAppliedJob
+{
+    public String job_id;
+    public String job_title;
+    public String status;
+    public boolean String;
+    public String company;
+
+    public SeekAppliedJob(JSONObject node)
+    {
+
+        JSONObject job = (JSONObject) node.optQuery("/job");
+        if(job == null)
+            return;
+        String title = job.optString("title");
+        //System.out.printf("Title: %s", title);
+
+        job_title = title;
+    }
+}
+
