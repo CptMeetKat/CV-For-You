e8067981c518cecffc0afe86ca51f63851ef0e86
Reece <reece.cares@gmail.com>
2024-09-19 20:40:03 +1000

    Added logging to CSVGenerator

--- src/main/java/MK/CVForYou/CSVGenerator.java
+++ src/main/java/MK/CVForYou/CSVGenerator.java
@@ -3,8 +3,13 @@ package MK.CVForYou;
 import java.lang.reflect.Field;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CSVGenerator
 {
+    static final Logger logger = LoggerFactory.getLogger(CSVGenerator.class);
+
     public static <T> String makeCSV(String columns[], List<T> items, Class<T> type)
     {
         StringBuilder sb = new StringBuilder();
@@ -21,9 +26,14 @@ public class CSVGenerator
                     Field field = type.getField(column);
                     String value = field.get(item).toString();
                     sb.append(String.format("'%s',", value));
-                } catch (IllegalAccessException | NoSuchFieldException e) {
+                } catch (IllegalAccessException e) {
                     sb.append("'null,'");
-                    e.printStackTrace();
+                    logger.warn("Unable to read field to generate CSV: {}", e.getMessage());
+                }
+                catch(NoSuchFieldException e)
+                {
+                    sb.append("'null,'");
+                    logger.warn("Field to generate CSV does not exist: {}", e.getMessage());
                 }
             }
             sb.append("\n");
