3771c29bd676d2da60931845deb3e2897255e5b3
Reece <reece.cares@gmail.com>
2024-09-20 20:08:22 +1000

    Add new flag to sample run file

--- run_sample.sh
+++ run_sample.sh
@@ -2,6 +2,6 @@
 
 
 sections=$(echo assets/sample_components/sections/*.json)
-mvn clean compile exec:java -Dexec.args="-d assets/sample_components/document.html -c assets/sample_components/description1.txt -s $sections"
+mvn clean compile exec:java -Dexec.args="-cv -d assets/sample_components/document.html -c assets/sample_components/description1.txt -s $sections"
 
 
