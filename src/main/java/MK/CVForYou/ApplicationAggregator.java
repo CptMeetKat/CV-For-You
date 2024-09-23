5b157929fbcb31bdace8e84eea097e84f34e479d
Reece <reece.cares@gmail.com>
2024-09-23 19:38:43 +1000

    Introduce Apache commons CSV to read CSVs

--- src/main/java/MK/CVForYou/ApplicationAggregator.java
+++ src/main/java/MK/CVForYou/ApplicationAggregator.java
@@ -1,63 +1,104 @@
 package MK.CVForYou;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.Reader;
+import java.util.Map;
+
 
 public class ApplicationAggregator
 {
     static String output = "data.csv";
     static final Logger logger = LoggerFactory.getLogger(App.class);
 
-    public static ArrayList<SeekAppliedJob> readData()
+
+    public static void _readData_apache()
     {
-        ArrayList<SeekAppliedJob> data = new ArrayList<>();
-        String csvFile = output;
-        String delimiter = ","; 
+        String filePath = "data.csv";
 
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+        ArrayList<SeekAppliedJob> applied_jobs = new ArrayList<SeekAppliedJob>();
 
-            String line = br.readLine();
-            if(line != null)
-            {
-                String[] csv_fields = line.split(delimiter);
+        try (
+            Reader reader = Files.newBufferedReader(Paths.get(filePath));
+            CSVParser csvParser = new CSVParser(reader, CSVFormat.Builder.create()
+                                                .setQuote('\'') 
+                                                .setIgnoreSurroundingSpaces(true) 
+                                                .setHeader() 
+                                                .setSkipHeaderRecord(true) 
+                                                .build())
+        ) {
+            Map<String, Integer> headerMap = csvParser.getHeaderMap();
+            List<String> headers = csvParser.getHeaderNames();
+            System.out.println("Headers: " + headerMap.keySet());
 
-                while ((line = br.readLine()) != null) {
-                    String[] csv_data = line.split(delimiter);
-
-                    SeekAppliedJob row = new SeekAppliedJob();
-                    for (int i = 0; i < csv_fields.length; i++) {
-
-                        String field = csv_fields[i];
-                        if (field.length() <= 2)
-                            continue;
-
-                        String field_trimmed = field.substring(1, field.length() - 1);
-
-                        try {
-                            Field f = SeekAppliedJob.class.getField(field_trimmed);
-                            if(f.getType() == Boolean.class || f.getType() == boolean.class)
-                                f.setBoolean(row, Boolean.getBoolean(csv_data[i]));
-                            else if(f.getType() == ArrayList.class)
-                                logger.warn("not implemented: did not populate array type"); //TODO: Incomplete
-                            else
-                                f.set(row, csv_data[i]);
-                        }
-                        catch(IllegalAccessException | NoSuchFieldException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    data.add(row);
-                }
+            for (CSVRecord row : csvParser) {
+                applied_jobs.add(createRecord(row, headers));
             }
-        } catch (IOException e) {
-            logger.error("{}", e.getMessage());
+        } catch (java.nio.charset.MalformedInputException e) {
+            logger.error(e.getMessage());
+        } catch(IOException e)
+        {
+            logger.error(e.getMessage());
         }
-        return data;
+    }
+
+    private static SeekAppliedJob createRecord(CSVRecord row, List<String> headers)
+    {
+        SeekAppliedJob record = new SeekAppliedJob();
+
+        for (String header : headers) {
+            String cell = row.get(header);
+            //logger.trace("data: {}", cell);
+            
+            try 
+            {
+                //logger.trace("Create record, writing field: {}\n", header);
+                Field f = SeekAppliedJob.class.getField(header);
+
+                if(f.getType() == Boolean.class || f.getType() == boolean.class)
+                    f.setBoolean(record, Boolean.getBoolean(cell));
+                else if(f.getType() == ArrayList.class)
+                    f.set(record, _cellToArrayList(cell));
+                else
+                    f.set(record, cell);
+            }
+            catch(IllegalAccessException | NoSuchFieldException e) {
+                logger.error(e.getMessage());
+            }
+
+        }
+
+        System.out.println(record);
+        return record;
+    }
+
+//TODO: need to know if empty cell is null or "" or "null"
+    private static ArrayList<String> _cellToArrayList(String cell_data)
+    {
+        ArrayList<String> result = new ArrayList<String>();
+
+        if(cell_data.length() > 2) //peel []
+        {
+            String[] atoms = cell_data.substring(1, cell_data.length() - 1)
+                                .split(",");
+
+            for (String a : atoms) {
+                result.add(new String(a.trim()));
+            }
+        }
+
+        return result;
     }
 }
