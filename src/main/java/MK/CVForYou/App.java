387b22ff0cd7de13950cfb1fa2db3f106166d367
Reece <reece.cares@gmail.com>
2024-10-02 15:06:37 +1000

    Seperate CV generating application from main

--- src/main/java/MK/CVForYou/App.java
+++ src/main/java/MK/CVForYou/App.java
@@ -1,11 +1,7 @@
 package MK.CVForYou;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 public class App 
 {
@@ -16,36 +12,8 @@ public class App
         ArgParser ap = new ArgParser();
         int mode = ap.parseArgs(args);
         if ( mode == 1 )
-            new App(ap);
+            new CVGeneratorApplication(ap);
         else if( mode == 2)
             new SeekStatsApplication();
-    }
-
-
-    public App(ArgParser ap)
-    { //TODO: Move to its own file
-        HashMap<String, InputJob> job_descriptions = getJobs(ap.getJDSource());
-
-
-        for (String job_id: job_descriptions.keySet()) 
-        {
-            DocumentGenerator generator = new DocumentGenerator(ap.getInputDocument(),
-                                    ap.getSections(),
-                                    ap.getOutputFolder());
-            generator.generateDocument(job_descriptions.get(job_id), job_id); //TODO: UGLY! FIX!
-            ExecuteChromePDFGenerator.run(job_id, ap.getOutputFolder());
-        }
-    }
-
-    public static HashMap<String, InputJob> getJobs(JobSource jd_source)
-    {
-        HashMap<String, InputJob> job_descriptions = new HashMap<String, InputJob>();
-        ArrayList<InputJob> jobs = jd_source.getJobModel(); 
-        for (InputJob job : jobs) {
-            if(job.name != null)
-                job_descriptions.put(job.name, job);
-        }
-
-        return job_descriptions;
     }
 }
---
+++ src/main/java/MK/CVForYou/CVGeneratorApplication.java
@@ -0,0 +1,33 @@
+package MK.CVForYou;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CVGeneratorApplication
+{
+    public CVGeneratorApplication(ArgParser ap)
+    {
+        HashMap<String, InputJob> job_descriptions = getJobs(ap.getJDSource());
+
+        for (String job_id: job_descriptions.keySet()) 
+        {
+            DocumentGenerator generator = new DocumentGenerator(ap.getInputDocument(),
+                                    ap.getSections(),
+                                    ap.getOutputFolder());
+            generator.generateDocument(job_descriptions.get(job_id), job_id); //TODO: UGLY! FIX!
+            ExecuteChromePDFGenerator.run(job_id, ap.getOutputFolder());
+        }
+    }
+
+    public static HashMap<String, InputJob> getJobs(JobSource jd_source)
+    {
+        HashMap<String, InputJob> job_descriptions = new HashMap<String, InputJob>();
+        ArrayList<InputJob> jobs = jd_source.getJobModel(); 
+        for (InputJob job : jobs) {
+            if(job.name != null)
+                job_descriptions.put(job.name, job);
+        }
+
+        return job_descriptions;
+    }
+}
