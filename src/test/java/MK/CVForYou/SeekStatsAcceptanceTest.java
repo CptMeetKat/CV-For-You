432832aad5c14337b1ca98f57d3049e0568d86a7
Reece <reece.cares@gmail.com>
2024-10-04 16:24:02 +1000

    Added acceptance test for merge CSV

--- src/test/java/MK/CVForYou/SeekStatsAcceptanceTest.java
+++ src/test/java/MK/CVForYou/SeekStatsAcceptanceTest.java
@@ -100,7 +100,41 @@ public class SeekStatsAcceptanceTest
 
 
     @Test
-    public void shouldMergeDataWithAlreadyExistingCSV()
+    public void shouldMergeDataWithAlreadyExistingCSV() throws IOException
     {
+        String initial_csv = "'job_id','job_title','status','status_times','latest_status','latest_status_time','active','company_name','company_id','applied_at','created_at','applied_with_cv','applied_with_cover','isExternal','salary','applicant_count','applicants_with_resume_percentage','applicants_with_cover_percentage'\n'1111','','','','','','false','','','2024-08-23T10:58:11.322Z','2024-08-23T05:46:20.186Z','false','false','false','','55','55','54'";
+        File file = new File(testDirectory.toString(), "data.csv");
+        Files.writeString(Paths.get(file.toString()), initial_csv);
+
+
+        String[] args = {"--seek-stats", "-a", "-o", file.toString()};
+        App app = new App(args, false);
+        Application program = app.getApplication();
+
+        ArrayList<SeekAppliedJob> applied_jobs = new ArrayList<>();
+        SeekAppliedJobWrapperMock applied_jobs_mock = new SeekAppliedJobWrapperMock(applied_jobs);
+        SeekAppliedJob application = new SeekAppliedJob(); //TODO: End to end test is more valuable if I utilise the JSONObject constructor
+        application.job_id = "1111";
+        application.created_at = "2024-08-23T05:46:20.186Z";
+        application.applied_at = "2024-08-23T10:58:11.322Z";
+        applied_jobs.add(application);
+	
+
+        SeekAppliedJobInsights insights = new SeekAppliedJobInsights(); //TODO: End to end test is more valuable if I utilise the JSONObject constructor
+        insights.applicant_count = 1;
+        insights.applicants_with_cover_percentage = 3;
+        insights.applicants_with_resume_percentage = 2;
+        SeekAppliedJobInsightsWrapperMock insights_mock = new SeekAppliedJobInsightsWrapperMock(insights);
+        
+        program.setDependency(applied_jobs_mock, SeekAppliedJobSource.class);
+        program.setDependency(insights_mock, SeekAppliedJobInsightsSource.class);
+
+        program.run();
+
+        String written = Files.readString(Paths.get(file.toString()));
+
+        String expected = "'job_id','job_title','status','status_times','latest_status','latest_status_time','active','company_name','company_id','applied_at','created_at','applied_with_cv','applied_with_cover','isExternal','salary','applicant_count','applicants_with_resume_percentage','applicants_with_cover_percentage'\n'1111','','','','','','false','','','2024-08-23T10:58:11.322Z','2024-08-23T05:46:20.186Z','false','false','false','','1','2','3'";
+        assertEquals(expected, written);
+
     }
 }
