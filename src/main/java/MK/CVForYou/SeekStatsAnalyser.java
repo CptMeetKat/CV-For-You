85afe2eb558e4c44fd4d607621df1a4087f76fc2
Reece <reece.cares@gmail.com>
2025-01-08 23:29:08 +1100

    Added tracking total internal & external applications

--- src/main/java/MK/CVForYou/SeekStatsAnalyser.java
+++ src/main/java/MK/CVForYou/SeekStatsAnalyser.java
@@ -1,5 +1,6 @@
 package MK.CVForYou;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SeekStatsAnalyser
@@ -7,6 +8,8 @@ public class SeekStatsAnalyser
     List<SeekAppliedJobCSVRow> applications; 
 
     int total_applications;
+    int total_internal_applications;
+    int total_external_application;
 
     public SeekStatsAnalyser(List<SeekAppliedJobCSVRow> applied_jobs)
     {
@@ -17,19 +20,25 @@ public class SeekStatsAnalyser
     public void generateStats()
     {
         total_applications = applications.size();
-       // for(SeekAppliedJobCSVRow row : applications)
-       // {
-       //     //System.out.println(row.job_title);
-       // }
+        ArrayList<SeekAppliedJobCSVRow> internal_applications = new ArrayList<SeekAppliedJobCSVRow>();
 
+        for(SeekAppliedJobCSVRow row : applications) {
+            if(!row.isExternal)
+                internal_applications.add(row);
+        }
 
-        //First remove isExternal
+        total_internal_applications = internal_applications.size();
+        total_external_application = total_applications - total_internal_applications;
+
+        //long count = applications.stream().filter(row -> row.field == 1).count();
     }
 
     public void printStats() //TODO: should this use logger or just system.out?
     {
         System.out.println("****Seek Job Application Stats****");
         System.out.printf("Total Application: %d%n", total_applications);
+        System.out.printf("Internal Applications: %d%n", total_internal_applications);
+        System.out.printf("External Applications: %d%n", total_external_application);
 
     }
 }
