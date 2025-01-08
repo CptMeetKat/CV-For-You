f39e09e09d43641f3692648c40438cd1d8acc042
Reece <reece.cares@gmail.com>
2025-01-09 00:07:13 +1100

    Added percentages for opened, viewed, rejected

--- src/main/java/MK/CVForYou/SeekStatsAnalyser.java
+++ src/main/java/MK/CVForYou/SeekStatsAnalyser.java
@@ -15,6 +15,12 @@ public class SeekStatsAnalyser
     long total_unopened;
     long total_viewed;
     long total_rejected;
+    long total_opened;
+
+    double total_opened_percentage;
+    double total_unopened_percentage;
+    double total_viewed_percentage;
+    double total_rejected_percentage;
 
 
     public SeekStatsAnalyser(List<SeekAppliedJobCSVRow> applied_jobs)
@@ -40,7 +46,13 @@ public class SeekStatsAnalyser
         total_unopened = applications.stream().filter(row -> row.latest_status.equals("Applied") == true).count();
         total_viewed = applications.stream().filter(row -> row.latest_status.equals("Viewed") == true).count();
         total_rejected = applications.stream().filter(row -> row.latest_status.equals("NotSuitable") == true).count();
+        total_opened = total_applications - total_unopened;
 
+        total_unopened_percentage = (double) total_unopened / total_applications;
+
+        total_opened_percentage = (double) total_opened / total_applications;
+        total_viewed_percentage = (double) total_viewed / total_applications;
+        total_rejected_percentage = (double) total_rejected / total_applications;
     }
 
     public void printStats() //TODO: should this use logger or just system.out?
@@ -53,9 +65,9 @@ public class SeekStatsAnalyser
         System.out.printf("%n****Internal Stats****%n");
         System.out.printf("\tPending Applications: %d / %d%n", total_open_applications, total_applications);
 
-        System.out.printf("\tUnopened: %d / %d (%f)%n", total_unopened, total_applications, (double) total_unopened / total_applications);
-        System.out.printf("\tOpened: %d / %d%n", total_applications - total_unopened, total_applications);
-        System.out.printf("\tViewed: %d / %d%n", total_viewed, total_applications);
-        System.out.printf("\tRejected: %d / %d%n", total_rejected, total_applications);
+        System.out.printf("\tUnopened: %d / %d (%f)%n", total_unopened, total_applications, total_unopened_percentage);
+        System.out.printf("\tOpened: %d / %d (%f)%n", total_opened, total_applications, total_opened_percentage);
+        System.out.printf("\tViewed: %d / %d (%f)%n", total_viewed, total_applications, total_viewed_percentage);
+        System.out.printf("\tRejected: %d / %d (%f)%n", total_rejected, total_applications, total_rejected_percentage);
     }
 }
