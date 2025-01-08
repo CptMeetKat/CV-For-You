ae6dae219a6196c0fad7f5ccd6c7a6c198a3e0a7
Reece <reece.cares@gmail.com>
2025-01-08 23:18:34 +1100

    Added tracking total applications

--- src/main/java/MK/CVForYou/SeekStatsAnalyser.java
+++ src/main/java/MK/CVForYou/SeekStatsAnalyser.java
@@ -5,16 +5,31 @@ import java.util.List;
 public class SeekStatsAnalyser
 {
     List<SeekAppliedJobCSVRow> applications; 
+
+    int total_applications;
+
     public SeekStatsAnalyser(List<SeekAppliedJobCSVRow> applied_jobs)
     {
         applications = applied_jobs;
+        generateStats();
     }
 
-    public void printStats()
+    public void generateStats()
     {
-        for(SeekAppliedJobCSVRow row : applications)
-        {
-            System.out.println(row.job_title);
-        }
+        total_applications = applications.size();
+       // for(SeekAppliedJobCSVRow row : applications)
+       // {
+       //     //System.out.println(row.job_title);
+       // }
+
+
+        //First remove isExternal
+    }
+
+    public void printStats() //TODO: should this use logger or just system.out?
+    {
+        System.out.println("****Seek Job Application Stats****");
+        System.out.printf("Total Application: %d%n", total_applications);
+
     }
 }
