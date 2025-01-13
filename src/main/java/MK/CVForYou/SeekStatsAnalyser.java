1f7f06615c5f67d7ec53be4e110424ff55b96b53
Reece <reece.cares@gmail.com>
2025-01-13 17:51:01 +1100

    Change word average -> mean

--- src/main/java/MK/CVForYou/SeekStatsAnalyser.java
+++ src/main/java/MK/CVForYou/SeekStatsAnalyser.java
@@ -209,9 +209,9 @@ public class SeekStatsAnalyser
 
 
         System.out.printf("%n****All Internal Applicants****%n");
-        System.out.printf("\tAverage Cover Letters: %.2f%%%n",  mean_cover_letter_percentage);
-        System.out.printf("\tAverage CV's: %.2f%%%n",  mean_cv_percentage);
-        System.out.printf("\tAverage applicants's: %.2f%n",  mean_total_applicants);
+        System.out.printf("\tMean Cover Letters: %.2f%%%n",  mean_cover_letter_percentage);
+        System.out.printf("\tMean CV's: %.2f%%%n",  mean_cv_percentage);
+        System.out.printf("\tMean applicants's: %.2f%n",  mean_total_applicants);
 
 
         System.out.printf("%n****Muliple Application****%n");
