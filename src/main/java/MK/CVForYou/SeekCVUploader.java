f978cf4db9e9bd2f047252d3f38b322d3641f532
Reece <reece.cares@gmail.com>
2025-01-14 23:16:42 +1100

    Created structure for obtaining the upload params

---
+++ src/main/java/MK/CVForYou/SeekCVUploader.java
@@ -0,0 +1,16 @@
+package MK.CVForYou;
+
+
+public class SeekCVUploader
+{
+    public SeekCVUploader() {}
+
+    public void uploadFile(String file)
+    {
+    }
+
+    public SeekDocumentUploadFormData getDocumentUploadParams()
+    {
+        return new SeekDocumentUploadFormData();
+    }
+}
---
+++ src/main/java/MK/CVForYou/SeekDocumentUploadFormData.java
@@ -0,0 +1,15 @@
+
+package MK.CVForYou;
+
+public class SeekDocumentUploadFormData
+{
+    public String key;
+    public String x_amz_algorithm;
+    public String x_amz_credential;
+    public String x_amz_date;
+    public String x_amz_security_token;
+    public String policy;
+    public String x_amz_signature;
+    public String x_amz_meta_filename;
+    public String x_amz_meta_candidateid;
+}
