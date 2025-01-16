50aa304e1a74af685211272d4e2f598c1f3fe130
Reece <reece.cares@gmail.com>
2025-01-16 20:21:20 +1100

    Multiple files can be included for upload

--- src/main/java/MK/CVForYou/CVUploaderArgs.java
+++ src/main/java/MK/CVForYou/CVUploaderArgs.java
@@ -2,14 +2,20 @@
 package MK.CVForYou;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class CVUploaderArgs
 {
-    public File file;
+    public ArrayList<File> files = new ArrayList<File>();
     public int mode;
 
     public void setMode(int mode)
     {
         this.mode = mode;
+    }
+
+    public void addFile(String filepath)
+    {
+        files.add(new File(filepath));
     }
 }
