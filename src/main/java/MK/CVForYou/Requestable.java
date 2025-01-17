b0b94c16808480e0c3daa26db3c6e826980c8760
Reece <reece.cares@gmail.com>
2025-01-17 19:21:45 +1100

    Added todo

--- src/main/java/MK/CVForYou/Requestable.java
+++ src/main/java/MK/CVForYou/Requestable.java
@@ -7,5 +7,5 @@ import org.json.JSONObject;
 
 public interface Requestable
 {
-    public JSONObject request(String access_token) throws IOException, InterruptedException;
+    public JSONObject request(String access_token) throws IOException, InterruptedException; //TODO Requestable should have been written to return HTTPResponse<String> instead of just the bodys
 }
