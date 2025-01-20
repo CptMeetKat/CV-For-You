fd0b040b968b533b19025a7dafae7723c57fcb6f
Reece <reece.cares@gmail.com>
2025-01-20 16:43:11 +1100

    Add variable for log reuse, change trace to debug log

--- src/main/java/MK/CVForYou/SeekResumeWrapper.java
+++ src/main/java/MK/CVForYou/SeekResumeWrapper.java
@@ -44,19 +44,20 @@ public class SeekResumeWrapper implements Requestable
 
     public JSONObject fetchSeekResumes(String access_token) throws IOException, InterruptedException
     {
+        String operation = "GetResumes";
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create("https://www.seek.com.au/graphql"))
             .header("accept", "*/*")
             .header("accept-language", "en-GB,en-US;q=0.9,en;q=0.8")
             .header("authorization", "Bearer " + access_token)
             .header("content-type", "application/json")
-            .method("POST", HttpRequest.BodyPublishers.ofString("{\"operationName\":\"GetResumes\",\"variables\":{\"languageCode\":\"en\"},\"query\":\"query GetResumes($languageCode: LanguageCodeIso\u0021) {\\n  viewer {\\n    _id\\n    resumes(languageCode: $languageCode) {\\n      id\\n      createdDateUtc\\n      isDefault\\n      fileMetadata {\\n        name\\n        size\\n        virusScanStatus\\n        sensitiveDataInfo {\\n          isDetected\\n          __typename\\n        }\\n        uri\\n        __typename\\n      }\\n      origin {\\n        type\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\"}"))
+            .method("POST", HttpRequest.BodyPublishers.ofString("{\"operationName\":\"" + operation + "\",\"variables\":{\"languageCode\":\"en\"},\"query\":\"query GetResumes($languageCode: LanguageCodeIso\u0021) {\\n  viewer {\\n    _id\\n    resumes(languageCode: $languageCode) {\\n      id\\n      createdDateUtc\\n      isDefault\\n      fileMetadata {\\n        name\\n        size\\n        virusScanStatus\\n        sensitiveDataInfo {\\n          isDetected\\n          __typename\\n        }\\n        uri\\n        __typename\\n      }\\n      origin {\\n        type\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\"}"))
             .build(); 
 
             logger.info("Fetching seek resumes...");
             HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
 
-            logger.trace("({} {} {}) {}", response.request().method(), response.uri(), "GetResumes", response.statusCode());
+            logger.debug("({} {} {}) {}", response.request().method(), response.uri(), operation, response.statusCode());
 
             return new JSONObject(response.body());
     }
