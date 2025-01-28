3b94090326274254315325667edf9d1f76b7e7ac
Reece <reece.cares@gmail.com>
2025-01-28 23:10:29 +1100

    Create args object for CVGeneration

---
+++ src/main/java/MK/CVForYou/CVGenerationArgs.java
@@ -0,0 +1,31 @@
+
+package MK.CVForYou;
+
+import java.nio.file.*;
+
+public class CVGenerationArgs
+{
+
+    JobSource jd_source; 
+    Path input_document; 
+    Path[] section_definition_paths;
+    Path output_directory;
+
+    public JobSource getJDSource()
+    {
+        return jd_source;
+    }
+
+    public Path getInputDocument()
+    {
+        return input_document;
+    }
+
+    public Path[] getSections() {
+        return section_definition_paths;
+    }
+
+    public Path getOutputFolder() {
+        return output_directory;
+    }
+}
