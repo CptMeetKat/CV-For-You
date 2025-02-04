3afc573f1c748566b4ab08dcc76b39aec297d1c9
Reece <reece.cares@gmail.com>
2025-02-04 21:32:22 +1100

    Added days since applied in updated record print

--- src/main/java/MK/CVForYou/UpdatedRecordFormatter.java
+++ src/main/java/MK/CVForYou/UpdatedRecordFormatter.java
@@ -1,9 +1,12 @@
 package MK.CVForYou;
 
 import java.lang.reflect.Field;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 public class UpdatedRecordFormatter
 {
+
     public static String format(SeekAppliedJobCSVRow former, SeekAppliedJobCSVRow updated)
     {
         //Class<?> clazz = SeekAppliedJobCSVRow.class;
@@ -13,7 +16,12 @@ public class UpdatedRecordFormatter
         sb.append(String.format(" %-10s %s\n", "job_id", updated.job_id));
         sb.append(String.format(" %-10s %s\n", "title", updated.job_title));
         sb.append(String.format(" %-10s %s\n", "company", updated.company_name));
-        sb.append(String.format(" %-10s %s; %s\n\n", "state", updated.latest_status, updated.active ? "Active":"Inactive"));
+        sb.append(String.format(" %-10s %s; %s\n", "state", updated.latest_status, updated.active ? "Active":"Inactive"));
+
+        long daysSince = getDaysSince(updated.applied_at);
+        sb.append(String.format(" %-10s %d %s ago\n\n", "applied", daysSince, daysSince != 1L ? "days": "day"));
+
+
  
 
         for (Field field : SeekAppliedJobCSVRow.class.getDeclaredFields()) {
@@ -30,5 +38,14 @@ public class UpdatedRecordFormatter
         }
 
         return sb.toString();
+    }
+
+
+    private static long getDaysSince(String date)
+    {
+        Instant applied = Instant.parse(date);
+        Instant now = Instant.now();
+        long daysSince = ChronoUnit.DAYS.between(applied, now);
+        return daysSince;
     }
 }
