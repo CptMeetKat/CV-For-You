657f9c0f274be42792a9602c5a5b0b5af1faa403
Reece <reece.cares@gmail.com>
2025-02-06 14:57:33 +1100

    Move formatters to local

--- src/main/java/MK/CVForYou/SeekResumesDeleteMenu.java
+++ src/main/java/MK/CVForYou/SeekResumesDeleteMenu.java
@@ -9,8 +9,6 @@ public class SeekResumesDeleteMenu implements Menu
     private String example_command_prefix;
     private static final String SEEK_RESUMES_DELETE_USAGE = "-id ID1 [ID2 ID3 ...]";
 
-    private HelpFormatter formatter = new HelpFormatter();
-    private CommandLineParser parser = new DefaultParser();
     private static final Logger logger = LoggerFactory.getLogger(SeekResumesDeleteMenu.class);
     
     public SeekResumesDeleteMenu(String example_command_prefix)
@@ -22,6 +20,7 @@ public class SeekResumesDeleteMenu implements Menu
 	public Application parse(String[] args) throws ParseException {
         try
         {
+            CommandLineParser parser = new DefaultParser();
             CommandLine cmd = parser.parse(getOptions(), args);
             if(cmd.hasOption("h") || args.length == 0) {
                 printExampleCommand();
@@ -63,6 +62,7 @@ public class SeekResumesDeleteMenu implements Menu
 
     private void printExampleCommand()
     {
+        HelpFormatter formatter = new HelpFormatter();
         formatter.printHelp(example_command_prefix + " " + SEEK_RESUMES_DELETE_USAGE, getOptions());
     }
 }
