1e7412e0b73d63610be22e5b6107eb9a0b50f353
Reece <reece.cares@gmail.com>
2025-04-28 22:47:03 +1000

    Capture the name of repo pr's come from

--- src/main/java/MK/CVForYou/CVModules/GitHubPullRequests.java
+++ src/main/java/MK/CVForYou/CVModules/GitHubPullRequests.java
@@ -57,8 +57,10 @@ public class GitHubPullRequests
             String title = pr.getString("title");
             String author_association = pr.getString("author_association");
             String state = pr.getString("state");
+            String repository_url = pr.getString("repository_url");
+            String repo_name = repository_url.substring(repository_url.lastIndexOf("/")+1); //TODO: This could be made safer
             if(author_association.equals("CONTRIBUTOR"))
-                System.out.printf("%s: %s\n", state, title);
+                System.out.printf("%s %s: %s\n", repo_name, state, title);
         }
     }
 
