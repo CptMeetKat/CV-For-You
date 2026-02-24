5a1a58747f6cc028299323bd04cb887050d5abba
Reece <reece.cares@gmail.com>
2026-02-24 15:56:04 +1100

    print all unique words

--- src/main/java/MK/CVForYou/SeekKeywordGeneratorApplication.java
+++ src/main/java/MK/CVForYou/SeekKeywordGeneratorApplication.java
@@ -3,7 +3,8 @@ package MK.CVForYou;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-
+import java.util.HashSet;
+import java.util.List;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.stream.Stream;
@@ -21,17 +22,25 @@ public class SeekKeywordGeneratorApplication implements Application
         ArrayList<Path> seek_jobs = getAllFilesInFolder(cache_directory);
         JobFromCache cached_jobs = new JobFromCache(seek_jobs);
         ArrayList<InputJob> jobs = cached_jobs.getJobModel();
+        List<String> unique_words = getAllUniqueWords(jobs);
 
-        for(InputJob job : jobs) {
-            System.out.println(job.job_title);
-        }
+        System.out.println(String.join("\n", unique_words));
 	}
 
-   // private void getAllUniqueWordsInJobDescription(Path seek_job_html)
-   // {
-   //     ArrayList<String> paths = getAllSeekJobsInFolder(cache_directory);
-   //     //SeekJobParser
-   // }
+    private List<String> getAllUniqueWords(ArrayList<InputJob> jobs)
+    {
+        HashSet<String> word_map = new HashSet<>();
+        for (InputJob job : jobs) {
+            if(job.job_description != null)
+            {
+                String[] split_text = job.job_description.split("[-., ]");
+                for(String word : split_text) {
+                    word_map.add(word);
+                }
+            }
+        }
+        return new ArrayList<>(word_map);
+    }
     
     private ArrayList<Path> getAllFilesInFolder(Path folder)
     {
