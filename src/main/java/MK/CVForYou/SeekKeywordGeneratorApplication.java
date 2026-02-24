4340c6bc712f1cee983050e4714f5537376749e7
Reece <reece.cares@gmail.com>
2026-02-24 16:38:56 +1100

    count all words

--- src/main/java/MK/CVForYou/SeekKeywordGeneratorApplication.java
+++ src/main/java/MK/CVForYou/SeekKeywordGeneratorApplication.java
@@ -3,8 +3,11 @@ package MK.CVForYou;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.stream.Stream;
@@ -22,27 +25,39 @@ public class SeekKeywordGeneratorApplication implements Application
         ArrayList<Path> seek_jobs = getAllFilesInFolder(cache_directory);
         JobFromCache cached_jobs = new JobFromCache(seek_jobs);
         ArrayList<InputJob> jobs = cached_jobs.getJobModel();
-        List<String> unique_words = getAllUniqueWords(jobs);
+        List< Entry<String, Integer>> unique_words = getAllUniqueWords(jobs);
 
-        String data = String.join("\n", unique_words);
-        IOUtils.writeToFile(data, "./wordlist.txt");
+
+        for (Entry<String,Integer> e : unique_words) {
+            System.out.println(e.getValue() + " " + e.getKey() ); 
+        }
+
+        //String data = String.join("\n", unique_words);
+        //IOUtils.writeToFile(data, "./wordlist.txt");
 	}
 
-    private List<String> getAllUniqueWords(ArrayList<InputJob> jobs)
+    private ArrayList<Entry<String, Integer>> getAllUniqueWords(ArrayList<InputJob> jobs)
     {
-        HashSet<String> word_map = new HashSet<>();
+        HashMap<String, Integer> word_map = new HashMap<>();
         for (InputJob job : jobs) {
             if(job.job_description != null)
             {
                 String text = job.job_description.replaceAll("[^\\w\\s+-]", " ").toLowerCase();
                 String[] split_text = text.split("[-., \n]");
                 for(String word : split_text) {
-                    if(!word.strip().isEmpty())
-                        word_map.add(word.strip());
+                    String normalised_word = word.strip();
+
+                    if(!normalised_word.isEmpty())
+                    {
+                        if(word_map.containsKey(normalised_word))
+                            word_map.put(normalised_word, word_map.get(normalised_word) + 1);
+                        else
+                            word_map.put(normalised_word, 1);
+                    }
                 }
             }
         }
-        return new ArrayList<>(word_map);
+        return new ArrayList<Map.Entry<String, Integer>>(word_map.entrySet());
     }
     
     private ArrayList<Path> getAllFilesInFolder(Path folder)
